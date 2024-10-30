5784f8820b2cc9bef3db4b63d14cc202118e51da
masinde sylvester <masinde.sylvester@yahoo.com>
2024-10-31 00:33:55 +0300

    initial commit

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -1,7 +1,7 @@
 import { username, city, image } from "../data/user";
 
 test("username, city, and image are all exported", () => {
-  expect(username).toEqual("Liza");
-  expect(city).toEqual("New York");
+  expect(username).toEqual("Masinde");
+  expect(city).toEqual("Nairobi, Kenya");
   expect(image).toEqual("https://i.imgur.com/mV8PQxj.gif");
 });
--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,6 @@
-const username = "Liza";
-const city = "New York";
+export const username = "Masinde";
+export const city = "Nairobi, Kenya";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+export const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
